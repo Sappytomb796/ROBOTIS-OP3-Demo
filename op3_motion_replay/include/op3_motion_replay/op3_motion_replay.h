9114f34c13b150c0ab2c0e147b8550c5568440ef
jmiller511 <46859016+jmiller511@users.noreply.github.com>
2019-04-23 17:15:25 -0700

    Replay: added replay file read and write
    op3_motion_replay/include/op3_motion_replay/op3_motion_replay.h
    op3_motion_replay/src/op3_motion_replay.cpp
    
    Added the functionality to read in replays from their txt file
    and to write a replay to a txt file.  Also changed storage format
    of messages from maps to a vector of sensor_msgs::JointState
    
    Signed-off-by: Jordan Miller <jmm23@pdx.edu>

--- op3_motion_replay/include/op3_motion_replay/op3_motion_replay.h
+++ op3_motion_replay/include/op3_motion_replay/op3_motion_replay.h
@@ -5,6 +5,7 @@
 #include <ros/package.h>
 #include <sensor_msgs/JointState.h>
 #include <std_msgs/String.h>
+#include <vector>
 
 namespace robotis_op{
     class MotionReplay{
@@ -16,11 +17,21 @@ namespace robotis_op{
             const std::string MODULE_NAME;
 
             void jointCallback(const sensor_msgs::JointState::ConstPtr& msg);
+			
+			bool saveReplay(std::string replay_name);
+			bool loadReplay(std::string replay_name);
 
             ros::NodeHandle nh_;
             ros::Subscriber op3_joints_sub_;
 			
-			std::map<std::string, double> joint_angles_;
+			std::vector<sensor_msgs::JointState> joint_states_;
+			
+			/*
+			std::vector<std::string> joint_name_;
+			std::map<std::string, std::vector<double>> joint_position_;
+			std::map<std::string, std::vector<double>> joint_velocity_;
+			std::map<std::string, std::vector<double>> joint_effort_;
+			*/
     };
 
 }
--- op3_motion_replay/src/op3_motion_replay.cpp
+++ op3_motion_replay/src/op3_motion_replay.cpp
@@ -1,4 +1,8 @@
 #include "op3_motion_replay/op3_motion_replay.h"
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+
 
 namespace robotis_op
 {
@@ -8,7 +12,15 @@ namespace robotis_op
 	{
 		op3_joints_sub_ = nh_.subscribe("/robotis/present_joint_states", 1, &MotionReplay::jointCallback, this);
 		
-		joint_angles_.clear();
+		joint_states_.clear();
+		
+		// old method
+		/*
+		joint_name_.clear();
+		joint_position_.clear();
+		joint_velocity_.clear();
+		joint_effort_.clear();
+		*/
 	}
 
 	MotionReplay::~MotionReplay()
@@ -16,21 +28,128 @@ namespace robotis_op
 		
 	}
 	
-	void jointCallback(const sensor_msgs::JointState::ConstPtr& msg)
+	void MotionReplay::jointCallback(const sensor_msgs::JointState::ConstPtr& msg)
 	{
 		if(msg->name.size() == 0)
 			return;
 		
 		int num_states = msg->name.size();
 		
+		sensor_msgs::JointState new_msg;
+		
 		for(int i = 0; i < num_states; i++)
 		{
-			joint_angles_.insert(std::pair<std::string, double>(msg->name[i], msg->position[i]));
+			new_msg.name.push_back(msg->name[i]);
+			new_msg.position.push_back(msg->position[i]);
+			new_msg.velocity.push_back(msg->velocity[i]);
+			new_msg.effort.push_back(msg->effort[i]);
+			
+			joint_states_.push_back(new_msg);
+			new_msg.clear();
+			
+			// old method
+			/* 
+			joint_name_.push_back(msg->name[i]);
+			joint_position_[msg->name[i]].push_back(msg->position[i]);
+			joint_velocity_[msg->name[i]].push_back(msg->velocity[i]);
+			joint_effort_[msg->name[i]].push_back(msg->effort[i]);
+			*/
 			
 			// print for testing
 			// might need to convert position (pos * 180 / M_PI)
-			std::cout << msg->name[i] << " -> " << msg->position[i] << std::endl;
+			std::cout << msg->name[i] <<  std::endl;
+			std::cout << "position: " << msg->position[i] << std::endl;
+			std::cout << "velocity: " << msg->velocity[i] << std::endl;
+			std::cout << "effort: " << msg->effort[i] << std::endl;
 		}
 	}
 	
+	bool MotionReplay::saveReplay(std::string replay_name)
+	{
+		if(joint_states_.size() == 0)
+		{
+			std::cout << "ERROR: no replay data to save...\n";
+			return false;
+		}
+		
+		std::ofstream file;
+		file.open(replay_name);
+		
+		if(!file.is_open())
+		{
+			std::cout << "ERROR: failed to open " << replay_name << ".txt\n";
+			return false;
+		}
+		
+		for (std::vector<sensor_msgs::JointState>::const_iterator it = joint_states_.begin(); it != joint_states_.end(); ++it)
+		{
+			for (std::vector<double>::const_iterator v = it->begin(); v != it->end(); ++v)
+				file << *v << '\t';
+		}
+		
+		// old method
+		/*
+		file << replay_name << std::endl;
+		
+		for (std::vector<std::string>::const_iterator it = joint_name_.begin(); it != joint_name_.end(); ++it)
+		{
+			file << *it << std::endl;
+
+			for (std::vector<double>::const_iterator v = joint_position_[*it].begin(); v != joint_position_[*it].end(); ++v)
+				file << *v << '\t';
+	
+			file << std::endl;
+
+			for (std::vector<double>::const_iterator v = joint_velocoty_[*it].begin(); v != joint_velocity_[*it].end(); ++v)
+				file << *v << '\t';
+
+			file << std::endl;
+
+			for (std::vector<double>::const_iterator v = joint_effort_[*it].begin(); v != joint_effort_[*it].end(); ++v)
+				file << *v << '\t';
+		}
+		*/
+		
+		//file << std::endl;
+
+		file.close();
+		
+		return true;
+	}
+	
+	bool MotionReplay::loadReplay(std::string replay_name)
+	{
+		std::ifstream file;
+		file.open(replay_name);
+		
+		if(!file.is_open())
+		{
+			std::cout << "ERROR: failed to open " << replay_name << ".txt\n";
+			return false;
+		}
+		
+		std::string data;
+		std::string token;
+		char delimiter = '\t';
+		sensor_msgs::JointState msg;
+		
+		joint_states_.clear();
+		
+		while(getline(file, data))
+		{
+			std::stringstream stream(data);
+			
+			while(getline(data, token, delimiter))
+			{
+				msg.position.push_back(atof(token.c_str()));
+			}
+			
+			joint_states.push_back(tmp);
+			msg.clear();
+		}
+		
+		file.close();
+		
+		return true;
+	}
 }
