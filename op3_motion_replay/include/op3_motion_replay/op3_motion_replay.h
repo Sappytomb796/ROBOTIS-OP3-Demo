319ade4aed1463281e9a1adc65980285e34b0f08
liqMix <ybrady@pdx.edu>
2019-04-25 19:28:26 -0700

    Replay: Adjusted indentation, removed unused code
    Signed-off-by: Brady Young <ybrady@pdx.edu>

--- op3_motion_replay/include/op3_motion_replay/op3_motion_replay.h
+++ op3_motion_replay/include/op3_motion_replay/op3_motion_replay.h
@@ -8,38 +8,31 @@
 #include <vector>
 
 namespace robotis_op{
-    class MotionReplay{
-    public:
-        MotionReplay();
-         ~MotionReplay();
+	class MotionReplay{
+		public:
+			MotionReplay();
+			~MotionReplay();
 
-    protected:
-        const std::string MODULE_NAME;
+		protected:
+			const std::string MODULE_NAME;
 
-        void jointCallback(const sensor_msgs::JointState::ConstPtr& msg);
-	    void buttonCallback(const std_msgs::String::ConstPtr& msg);
-	    
-	    void publishJointStates();
-    
-	    bool saveReplay(std::string replay_name);
-	    bool loadReplay(std::string replay_name);
+			void jointCallback(const sensor_msgs::JointState::ConstPtr& msg);
+			void buttonCallback(const std_msgs::String::ConstPtr& msg);
 
-        ros::NodeHandle nh_;
-        ros::Subscriber op3_joints_sub_;
-        ros::Subscriber button_sub_;	
-        ros::Publisher joint_state_pub_;	
-	    std::vector<sensor_msgs::JointState> joint_states_;
-    	    
-	    /*
-	    std::vector<std::string> joint_name_;
-	    std::map<std::string, std::vector<double>> joint_position_;
-	    std::map<std::string, std::vector<double>> joint_velocity_;
-	    std::map<std::string, std::vector<double>> joint_effort_;
-	    */
-            
-        bool record_flag;
-	    std::map<std::string, double> joint_angles_;
-    };
+			void publishJointStates();
+
+			bool saveReplay(std::string replay_name);
+			bool loadReplay(std::string replay_name);
+
+			ros::NodeHandle nh_;
+			ros::Subscriber op3_joints_sub_;
+			ros::Subscriber button_sub_;	
+			ros::Publisher joint_state_pub_;	
+			std::vector<sensor_msgs::JointState> joint_states_;
+
+			bool record_flag;
+			std::map<std::string, double> joint_angles_;
+	};
 
 }
 
