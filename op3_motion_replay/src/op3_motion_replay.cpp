e1f76a480fdc22000ed98754ed1db2111f2f8ec5
jmiller511 <46859016+jmiller511@users.noreply.github.com>
2019-05-23 20:16:54 -0700

    Replay: Fixed load replay bug, modified loop rate
    op3_motion_replay/src/op3_motion_replay.cpp
    
    Load replay now successfully loads a replay and published the
    correct joint states.  Also modified the loop rate to make the
    replay movements move faster.
    
    Signed-off-by: Jordan Miller <jmm23@pdx.edu>

--- op3_motion_replay/src/op3_motion_replay.cpp
+++ op3_motion_replay/src/op3_motion_replay.cpp
@@ -12,60 +12,62 @@ namespace robotis_op
 		MODULE_NAME("op3_motion_replay")
 	{
 		op3_joints_sub_ = nh_.subscribe("/robotis/present_joint_states", 1,
-										&MotionReplay::jointCallback, this);
+				&MotionReplay::jointCallback, this);
 		web_sub_ = nh_.subscribe("/robotis/replay/web", 1, &MotionReplay::webCallback, this);
 
 		web_message_pub_ = nh_.advertise<std_msgs::String>("/robotis/replay/status", 0);
 		joint_state_pub_ = nh_.advertise<sensor_msgs::JointState>("/robotis/direct_control/set_joint_states", 0);
 		module_pub_ = nh_.advertise<std_msgs::String>("/robotis/enable_ctrl_module", 0);
-				
+
 		joint_states_.clear();
-	 	joint_angles_.clear();	
+		joint_angles_.clear();	
 		record_flag = false;
 	}
 
 	MotionReplay::~MotionReplay()
 	{
-		
+
 	}
-	
+
 	void MotionReplay::enableModule(std::string module_name)
 	{
 		std_msgs::String msg;
 		msg.data = module_name;
 		module_pub_.publish(msg);
 	}
-	
+
 	void MotionReplay::jointCallback(const sensor_msgs::JointState::ConstPtr& msg)
 	{
 		if(!record_flag)
 			return;
-			
+
 		if(msg->name.size() == 0)
 			return;
-		
+
 		int num_states = msg->name.size();
-		
+
 		sensor_msgs::JointState new_msg;
-		
+
 		for(int i = 0; i < num_states; i++)
 		{
 			new_msg.name.push_back(msg->name[i]);
 			new_msg.position.push_back(msg->position[i]);
 			new_msg.velocity.push_back(msg->velocity[i]);
 			new_msg.effort.push_back(msg->effort[i]);
-			
+
 			ROS_INFO("%s\nposition: %f\nvelocity: %f\neffort: %f\n",
-					 msg->name[i].c_str(), msg->position[i], msg->velocity[i], msg->effort[i]);
+					msg->name[i].c_str(), msg->position[i], msg->velocity[i], msg->effort[i]);
 		}
-		
+
 		joint_states_.push_back(new_msg);
 	}
 
 	void MotionReplay::webCallback(const std_msgs::MultiArrayDimension::ConstPtr& msg){
 		switch(msg->size){
 			case REPLAY_START:
-				joint_states_.clear();
+				if(record_flag == false)
+					joint_states_.clear();
+
 				record_flag = !record_flag;
 				ROS_INFO("Button: start");
 				break;
@@ -79,7 +81,7 @@ namespace robotis_op
 				loadReplay(msg->label);
 				ROS_INFO("Button: load");
 				break;
-			
+
 			case REPLAY_DELETE:
 				deleteReplay(msg->label);
 				ROS_INFO("Button: delete");
@@ -96,7 +98,7 @@ namespace robotis_op
 				break;
 		}
 	}
-	
+
 	void MotionReplay::publishJointStates()
 	{
 		std_msgs::String web_message;
@@ -109,18 +111,18 @@ namespace robotis_op
 			ROS_INFO("%s", message.c_str());
 			return;
 		}
-		
-		ros::Rate loop_rate(30);
+
+		ros::Rate loop_rate(15);
 
 		for (std::vector<sensor_msgs::JointState>::const_iterator it = joint_states_.begin();
-			 it != joint_states_.end(); ++it)
+				it != joint_states_.end(); ++it)
 		{
-			 joint_state_pub_.publish(*it);
-			 loop_rate.sleep();
-       		 ROS_INFO("%f: \n", (*it).position[0]);
+			joint_state_pub_.publish(*it);
+			loop_rate.sleep();
+			ROS_INFO("%f: \n", (*it).position[0]);
 		}
 	}
-	
+
 	bool MotionReplay::saveReplay(std::string replay_name)
 	{
 		std_msgs::String web_message;
@@ -134,11 +136,11 @@ namespace robotis_op
 			ROS_INFO("%s", message.c_str());
 			return false;
 		}
-		
+
 		ROS_INFO("Current path is %s", get_current_dir_name());
 		std::ofstream file;
 		file.open(replay_name + ".txt");
-		
+
 		if(!file.is_open())
 		{
 			message = "ERROR: failed to open " + replay_name + ".txt";
@@ -147,32 +149,37 @@ namespace robotis_op
 			ROS_INFO("%s", message.c_str());
 			return false;
 		}
-		
+
 		for (std::vector<sensor_msgs::JointState>::const_iterator it = joint_states_.begin();
-			 it != joint_states_.end(); ++it)
+				it != joint_states_.end(); ++it)
 		{
-			file << (*it).name << '\t';
-			file << (*it).position << '\t';
-			file << (*it).velocity << '\t';
-			file << (*it).effort;
+			int len = (*it).name.size();
+			for(int i = 0; i < len; i++)
+			{
+				file << (*it).name[i] << ' ';
+				file << (*it).position[i] << ' ';
+				file << (*it).velocity[i] << ' ';
+				file << (*it).effort[i] << '\t';
+			}
+
 			file << '\n';
-		/*
-			for (std::vector<double>::const_iterator pit = (*it).position.begin();
-				 pit != (*it).position.end(); ++pit)
-				file << *pit << '\t';
-			file << '\n';
-		*/
+			/*
+			   for (std::vector<double>::const_iterator pit = (*it).position.begin();
+			   pit != (*it).position.end(); ++pit)
+			   file << *pit << '\t';
+			   file << '\n';
+			 */
 		}
-		
+
 		file.close();
-		
+
 		message = replay_name + ".txt written.";
 		web_message.data = message;
 		web_message_pub_.publish(web_message);
 		ROS_INFO("%s", message.c_str());
 		return true;
 	}
-	
+
 	bool MotionReplay::loadReplay(std::string replay_name)
 	{
 		std_msgs::String web_message;
@@ -180,7 +187,7 @@ namespace robotis_op
 
 		std::ifstream file;
 		file.open(replay_name + ".txt");
-		
+
 		if(!file.is_open())
 		{
 			message = "ERROR: failed to open " + replay_name + ".txt";
@@ -189,34 +196,44 @@ namespace robotis_op
 			ROS_INFO("%s", message.c_str());
 			return false;
 		}
-		
+
 		std::string data;
 		std::string token;
-		char delimiter = '\t';
+		std::string state;
+		char delimiter_tab = '\t';
+		char delimiter_space = ' ';
 		sensor_msgs::JointState msg;
-		
+
 		joint_states_.clear();
-		
+
 		while(getline(file, data))
 		{
 			std::stringstream stream(data);
-			
-			getline(stream, token, delimiter);
-			msg.name.push_back(token);
-			
-			getline(stream, token, delimiter);
-			msg.position.push_back(atof(token.c_str()));
-			
-			getline(stream, token, delimiter);
-			msg.velocity.push_back(atof(token.c_str()));
-			
-			getline(stream, token);
-			msg.effort.push_back(atof(token.c_str()));
-			
+
+			while(getline(stream, state, delimiter_tab))
+			{
+				std::stringstream stream_state(state);
+
+				getline(stream_state, token, delimiter_space);
+				msg.name.push_back(token);
+
+				getline(stream_state, token, delimiter_space);
+				msg.position.push_back(atof(token.c_str()));
+
+				getline(stream_state, token, delimiter_space);
+				msg.velocity.push_back(atof(token.c_str()));
+
+				getline(stream_state, token);
+				msg.effort.push_back(atof(token.c_str()));
+			}
+
 			joint_states_.push_back(msg);
-			msg.clear();
+			msg.name.clear();
+			msg.position.clear();
+			msg.velocity.clear();
+			msg.effort.clear();
 		}
-		
+
 		file.close();
 		message = replay_name + ".txt loaded.";
 		web_message.data = message;
@@ -229,7 +246,7 @@ namespace robotis_op
 		std::string file_path = file_name + ".txt";
 		std_msgs::String web_message;
 		std::string message;
-		
+
 		if(remove(file_path.c_str()) == 0){
 			message = "Removed " + file_path;
 			web_message.data = message;
